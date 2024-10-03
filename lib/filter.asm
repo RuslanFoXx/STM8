3caf763cb5bff131fd574e87e5dc305af2c2c44f
FoXx <89214274+RuslanFoXx@users.noreply.github.com>
2024-10-03 13:25:47 +0300

    Low Filter

--- lib/filter.asm
+++ lib/filter.asm
@@ -10,74 +10,74 @@
 ;	ldw Low_Result, #Buffer_C
 ;------------------------------------------------------
 filter.l
-	ld		A, #LOW_BUFFER_SIZE
+	ld	A, #LOW_BUFFER_SIZE
 
 index@filtern.w
 	push	A
 
 	clrw	X
-	ldw		Low_integrator,  X	;	integrator = 0
-	ldw		Low_integrator2, X
+	ldw	Low_integrator,  X	;	integrator = 0
+	ldw	Low_integrator2, X
 
-	ld		A, [Low_Scale]
-	ld		Low_level, A
+	ld	A, [Low_Scale]
+	ld	Low_level, A
 
-	ldw		X, Low_Buffer
-	ld		A, #INTEGRATOR_FRAME_SIZE
+	ldw	X, Low_Buffer
+	ld	A, #INTEGRATOR_FRAME_SIZE
 
 integrator@filtern.w
 		push	A
 		pushw	X
 
-		ldw		X, (X)
-		ld		A, XH
-		ld		YL, A
-		ld		A, Low_level
-		mul		X, A			;	X = frame.b1 * scale[index]
-		mul		Y, A			;	Y = frame.b2 * scale[index]
+		ldw	X, (X)
+		ld	A, XH
+		ld	YL, A
+		ld	A, Low_level
+		mul	X, A		;	X = frame.b1 * scale[index]
+		mul	Y, A		;	Y = frame.b2 * scale[index]
 
-		ld		A, XL
-		add		A, Low_integrator ; integrator[0]+= XL
-		ld		Low_integrator, A
+		ld	A, XL
+		add	A, Low_integrator ; integrator[0]+= XL
+		ld	Low_integrator, A
 
-		ld		A, XH
-		adc		A, Low_integrator1
-		ld		Low_integrator1, A
+		ld	A, XH
+		adc	A, Low_integrator1
+		ld	Low_integrator1, A
 
-		ld		A, YL
-		adc		A, Low_integrator1 ; integrator[1]+= YL + XH
-		ld		Low_integrator1, A
+		ld	A, YL
+		adc	A, Low_integrator1 ; integrator[1]+= YL + XH
+		ld	Low_integrator1, A
 
-		ld		A, YH
-		adc		A, Low_integrator2 ; integrator[2]+= YH
-		ld		Low_integrator2, A
+		ld	A, YH
+		adc	A, Low_integrator2 ; integrator[2]+= YH
+		ld	Low_integrator2, A
 
-		clr		A
-		adc		A, Low_integrator3 ; integrator[3]+= carry
-		ld		Low_integrator3, A
+		clr	A
+		adc	A, Low_integrator3 ; integrator[3]+= carry
+		ld	Low_integrator3, A
 
-		inc		Low_level		;	scale++
+		inc	Low_level	;	scale++
 
-		popw	X				;	frame++
+		popw	X		;	frame++
 		incw	X
 
-		pop		A				;	index++
+		pop		A	;	index++
 		dec		A
 		jrne integrator@filtern
 
-	ldw		X, Low_integrator2	;	result[pos]= integrator >> 16
-	ldw		[Low_Result], X
+	ldw	X, Low_integrator2	;	result[pos]= integrator >> 16
+	ldw	[Low_Result], X
 
-	ldw		X, #2
+	ldw	X, #2
 	addw	X, Low_Result		;	result+= word_size
-;	ldw		Low_Result, X
+	ldw	Low_Result, X
 
-	ldw		X, #2
+	ldw	X, #2
 	addw	X, Low_Buffer		;	frame+= word_size
-;	ldw		Low_Buffer, X
+	ldw	Low_Buffer, X
 
-	pop		A
-	dec		A
+	pop	A
+	dec	A
 	jrne index@filtern
 
 	ret
